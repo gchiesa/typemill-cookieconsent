d42432b03d373504dace1fa512c755126ba9a517
Giuseppe Chiesa <mail@giuseppechiesa.it>
2025-05-06 18:27:28 +0200

    fix: add CORS hook directives

--- cookieconsent.php
+++ cookieconsent.php
@@ -7,33 +7,43 @@ use \Typemill\Models\Settings;
 
 class CookieConsent extends Plugin
 {
-	protected $settings;
-	
+    protected $settings;
+
     public static function getSubscribedEvents()
     {
-		return array(
-			'onSettingsLoaded'		=> 'onSettingsLoaded',
-			'onTwigLoaded' 			=> 'onTwigLoaded'
-		);
+        return array(
+            'onSettingsLoaded' => 'onSettingsLoaded',
+            'onTwigLoaded' => 'onTwigLoaded',
+            'onCspLoaded' => 'onCspLoaded',
+        );
     }
-	
-	public function onSettingsLoaded($settings)
-	{
-		$this->settings = $settings->getData();
-	}
-	
-	public function onTwigLoaded()
-	{
-        $twig 	= $this->getTwig();
-		$loader = $twig->getLoader();
-		$loader->addPath(__DIR__ . '/templates');
+
+    public function onSettingsLoaded($settings)
+    {
+        $this->settings = $settings->getData();
+    }
+
+    public function onTwigLoaded()
+    {
+        $twig = $this->getTwig();
+        $loader = $twig->getLoader();
+        $loader->addPath(__DIR__ . '/templates');
 
         $settings = $this->getPluginSettings();
 
-        $this->addJS('//www.unpkg.com/vanilla-cookieconsent@3.1.0/dist/cookieconsent.umd.js');
-		$this->addInlineJS($twig->fetch('/cookie-consent-js.twig', $settings));
+        $this->addJS('//unpkg.com/vanilla-cookieconsent@3.1.0/dist/cookieconsent.umd.js');
+        $this->addInlineJS($twig->fetch('/cookie-consent-js.twig', $settings));
 
         /* add CSS */
         $this->addCSS('https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.1.0/dist/cookieconsent.css');
-	}
+    }
+
+    public function onCspLoaded($csp) {
+        $data = $csp->getData();
+
+        $data[] = 'cdn.jsdelivr.net';
+        $data[] = 'unpkg.com';
+
+        $csp->setData($data);
+    }
 }
